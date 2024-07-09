2f409c69c8d408436d29c5e14baf4374717008b6
margyle <matt.coding@gmail.com>
2024-07-09 17:39:13 -0400

    update test routing

--- tests/test_pump_routes.py
+++ tests/test_pump_routes.py
@@ -3,16 +3,16 @@ from unittest.mock import patch, MagicMock
 from fastapi.testclient import TestClient
 from fastapi import FastAPI
 import pigpio  # Import pigpio for usage in your test
-
-from operator_app.api.v1.pump.pump_routes import router
+from operator_app.api.v1 import v1_router
 
 app = FastAPI()
-app.include_router(router)
+app.include_router(v1_router, prefix="/v1")
+
 client = TestClient(app)
 
 @patch('operator_app.api.v1.pump.pump_routes.pi')
 def test_control_pump(mocked_pi):
-  response = client.post("/", json={"direction": "forward"})
+  response = client.post("/v1/pump/", json={"direction": "forward"})
   assert response.status_code == 200
   mocked_pi.set_mode.assert_any_call(13, pigpio.OUTPUT)
   mocked_pi.set_mode.assert_any_call(19, pigpio.OUTPUT)
@@ -21,7 +21,7 @@ def test_control_pump(mocked_pi):
 
 @patch('operator_app.api.v1.pump.pump_routes.pi')
 def test_control_pump_speed(mocked_pi):
-  response = client.post("/flow-rate", json={"direction": "forward", "speed": 50})
+  response = client.post("/v1/pump/flow-rate", json={"direction": "forward", "speed": 50})
   assert response.status_code == 200
   mocked_pi.set_mode.assert_any_call(13, pigpio.OUTPUT)
   mocked_pi.set_mode.assert_any_call(19, pigpio.OUTPUT)
